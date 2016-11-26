1b21214f0d3dd2e0669640218870c73021de6b62
danielrpg <daniel.fernandezgp@gmail.com>
2016-11-25 19:51:14 -0600

    First Changes

---
+++ src/main/java/com/delivery/app/DeliveryAppApplication.java
@@ -0,0 +1,12 @@
+package com.delivery.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/delivery/app/DeliveryAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.delivery.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeliveryAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
